ffcca76e8326331671b7702327aa3943558ba7f2
Mark <50715288+whymeoff@users.noreply.github.com>
2020-05-17 15:20:06 +0300

    auth handling

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const passport = require('passport')
 const { registerUser } = require('../controllers/user')
 
 const router = express.Router()
 
 router.post('/register', registerUser)
-// router.post('/login')
-// router.post('/logout')
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    res.send()
+})
+
+router.post('/logout', (req, res) => {
+    req.logOut()
+    res.send()
+})
+
+router.get('/auth', (req, res) => res.send({ isAuth: req.isAuthenticated() }))
 
 module.exports = router
